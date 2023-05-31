a3619fd8efd5b436753f243824e1e9ec75c5f38d
ella-irene <heine@carleton.edu>
2023-05-30 20:54:20 -0500

    updated database

--- database/sampleQueries.sql
+++ database/sampleQueries.sql
@@ -1,10 +1,9 @@
 /* List all of the counties with reported cases of avian flu */
-SELECT counties FROM wildlifeMorbidityEvents WHERE eventDiagnosis like '%Avian Flu%';
+SELECT counties FROM wildlifemorbidityevents WHERE eventDiagnosis like '%Avian Influenza%';
 
-/* Get county with most affected animals in a single event */
-SELECT * FROM wildlifeMorbidityEvents ORDER BY numberAffected ASC
-SELECT TOP 1 * FROM wildlifeMorbidityEvents
-SELECT counties FROM wildlifeMorbidityEvents;
+/* Get county with most affected animals in a single event, along with the number affected */
+SELECT counties, numberAffected FROM wildlifeMorbidityEvents ORDER BY numberAffected DESC 
+LIMIT 1;
 
-/* Get all data for events in rice county starting in 2022 */
-SELECT * FROM wildlifeMorbidityEvents WHERE counties = 'Rice County, MN' and eventStartDate::date BETWEEN '2022-01-01' AND '2022-12-31';
+/* Get all data for events in rice county starting in 2011 */
+SELECT * FROM wildlifeMorbidityEvents WHERE counties = 'Rice County, MN' and eventStartDate::date BETWEEN '2011-01-01' AND '2012-12-31';
